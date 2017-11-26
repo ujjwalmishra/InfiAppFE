e19754a16c3b48c3ffb736f15ce58c7346b32b55
workspace <gusto@gistavs.attlocal.net>
2017-11-26 07:09:04 -0600

    sec view enlarged btns

--- src/containers/auth/CreateForm.js
+++ src/containers/auth/CreateForm.js
@@ -12,6 +12,8 @@ import {
 } from 'react-native';
 
 import { Actions } from 'react-native-router-flux';
+import AuthenticateView from '@containers/auth/AuthenticateView'
+
 // Consts and Libs
 import { AppStyles, AppSizes, AppColors } from '@theme/';
 
@@ -22,79 +24,51 @@ import { Spacer, Text,Button} from '@ui/';
 /* Styles ==
 /* Styles ==
 /* Styles ==================================================================== */
-const styles = StyleSheet.create({
-  background: {
-    backgroundColor: AppColors.brand.primary,
-    height: AppSizes.screen.height,
-    width: AppSizes.screen.width,
-  },
-  logo: {
-    width: AppSizes.screen.width * 0.6,
-    resizeMode: 'contain',
-  },
-  blue_arrow: {
-    width:  120,
-    height: 120,
-  },
-  whiteText: {
-    color: '#FFF',
-  },
-});
+export default class App extends Component<{}> {
+  render() {
+    return (
 
-/* Component ==================================================================== */
-class CreateForm extends Component {
-  static componentName = 'CreateForm';
 
-  render = () => (
+      <View style={styles.container}>
 
-    <View style={[AppStyles.containerCentered, AppStyles.container, styles.background]}>
+      <TextInput style={styles.input} placeholder= "Enter legal first Name"/>
+       <TextInput style={styles.input} placeholder= "Enter legal last Name"/>
+       <TextInput style={styles.input} placeholder= "Enter valid email"/>
+       <TextInput style={styles.input} placeholder= "Enter this Devide Number"/>
 
-      <Image
-        source={require('../../images/logo.png')}
-        style={[styles.logo]}
-      />
+       <Button
 
-      <Text style={{fontSize: 20,textAlign: 'center',color:'#000', margin: 20,fontWeight: 'bold'}}>
-        Secure identity management built on blockchain
-     </Text>
+       color={'#000'}
+       title='Create New Infinity ID'
+       onPress={Actions.app}
+       raised={false}
+       backgroundColor={'#d6d6d6'}
+       />
 
-        <Text style={{fontSize: 18,textAlign: 'center',color:'#000', margin: 20, fontFamily:'Cochin'}}>
-         Second page
-      </Text>
-
-<Spacer size={50} />
-
-      <View style={[AppStyles.row, AppStyles.paddingHorizontal,AppStyles.container,AppStyles.containerCentered]}>
-  
-        <View style={[AppStyles.flex1]} />
-          <Spacer size={35} />
-        <View style={[AppStyles.flex6]}>
-          <Button
-          color={'#000'}
-          title='Enable Touch ID'
-          onPress={Actions.app}
-          raised={false}
-          backgroundColor={'#d6d6d6'}
-          />
-
-        </View>
-          <Spacer size={15} />
-        <View style={[AppStyles.flex6]}>
-          <Button
-          color={'#000'}
-          title='Create New Passcode'
-          onPress={Actions.passcode}
-          raised={false}
-          backgroundColor={'#d6d6d6'}
-          />
-
-        </View>
-        <View style={[AppStyles.flex1]} />
       </View>
-
-    </View>
-  )
+    );
+  }
 }
 
-/* Export Component ==================================================================== */
-export default CreateForm;
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  input: {
+    fontSize: 12,
+    color: 'black',
+    textAlign: 'left',
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+    marginBottom: 10,
+    height: 40,
+    width: Dimensions.get('window').width*.86
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
